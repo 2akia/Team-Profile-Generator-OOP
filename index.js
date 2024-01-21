cb1e21cf8169a23879e3e82369b94e4b1408a013
2akia <2akssultana@gmail.com>
2024-01-21 20:00:51 +0000

    added the engineerinfo function/getintern info

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const teamMembers = [];
+
 function getManagerInfo() {
     return
     prompt([
@@ -49,10 +51,77 @@ function getManagerInfo() {
         });
 }
 
+function getEngineerInfo() {
+    return
 
+    prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: "Enter the engineer's name:",
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: "Enter the engineer's employee ID:",
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: "Enter the engineer's email address:",
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: "Enter the engineer's GitHub username:",
+        },
+      ])
+      .then((answers) => {
+        const engineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github
+        );
+        teamMembers.push(engineer);
+      });
+  }
 
-
-
+  function getInternInfo() {
+    return 
+      prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: "Enter the intern's name:",
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: "Enter the intern's employee ID:",
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: "Enter the intern's email address:",
+        },
+        {
+          type: 'input',
+          name: 'school',
+          message: "Enter the intern's school:",
+        },
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.school
+        );
+        teamMembers.push(intern);
+      });
+  }
+  
 
 
 
