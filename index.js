98e3ea10553315fbf61a88be8a3ae3fc4c3e1566
2akia <2akssultana@gmail.com>
2024-01-21 17:59:14 +0000

    added the getmanagerinfo function

--- index.js
+++ index.js
@@ -13,3 +13,52 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+function getManagerInfo() {
+    return
+    prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter the team manager's name:",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter the team manager's employee ID:",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter the team manager's email address:",
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Enter the team manager's office number:",
+        },
+
+    ])
+        .then((answers) => {
+            const manager = new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.officeNumber
+            );
+            teamMembers.push(manager);
+        });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
